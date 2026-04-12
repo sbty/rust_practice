48df84759f61638809e5ad3a3273aefe744dad74
sbty1459 <shibatay@protonmail.com>
2026-04-13 00:01:54 +0900

    chore: ignore cargo target

--- exercises/day02_variables_and_types/target/debug/day02_variables_and_types.d
+++
@@ -1,1 +0,0 @@
-C:\dev\rust_practice\exercises\day02_variables_and_types\target\debug\day02_variables_and_types.exe: C:\dev\rust_practice\exercises\day02_variables_and_types\src\main.rs
--- exercises/day02_variables_and_types/target/debug/deps/day02_variables_and_types.d
+++
@@ -1,5 +0,0 @@
-C:\dev\rust_practice\exercises\day02_variables_and_types\target\debug\deps\day02_variables_and_types.d: src\main.rs
-
-C:\dev\rust_practice\exercises\day02_variables_and_types\target\debug\deps\day02_variables_and_types.exe: src\main.rs
-
-src\main.rs:
